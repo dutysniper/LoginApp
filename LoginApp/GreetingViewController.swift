1b37904b37962d9a91858ec52b84ebf60b608179
dutysniper <natarov.constantin@yandex.ru>
2023-03-31 14:20:07 +0300

    create logic +
    create segue, unwind.
    create logic for actions.
    create alerts

--- LoginApp/GreetingViewController.swift
+++ LoginApp/GreetingViewController.swift
@@ -7,23 +7,14 @@
 
 import UIKit
 
-class GreetingViewController: UIViewController {
+final class GreetingViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    
+    var userName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        greetingLabel.text = "Hello, \(userName)"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -7,13 +7,58 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+final class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    private let testLogin = "login"
+    private let testPassword = "password"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let greetingVC = segue.destination as? GreetingViewController else
+        { return }
+        greetingVC.userName = usernameTF.text ?? ""
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        usernameTF.text = ""
+        passwordTF.text = ""
+    }
+    @IBAction func loginButtonPressed () {
+        if usernameTF.text != testLogin || passwordTF.text != testPassword {
+            alert(title: "Oops!", message: "Incorrect login/password")
+        }
+    }
+    
+    @IBAction func forgotNameButtonTapped() {
+        alert(title: "Hint💡", message: "Your username is login ")
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+        alert(title: "Hint💡", message: "Your password is password")
+    }
+    
+    private func alert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.passwordTF.text = ""
+        }))
+        present(alert, animated: true)
+        
+    }
 }
+    
+
 
